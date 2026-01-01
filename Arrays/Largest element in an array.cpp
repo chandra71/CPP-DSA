29c54f6afde1ff1344597bee1227011462d1f7f8
chandu_116 <118456799+chandra71@users.noreply.github.com>
2026-01-01 22:52:45 +0530

    Add files via upload

---
+++ Arrays/Largest element in an array.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[] = {1,3,4,5,6};
+    int n = 5;
+    int largest  = arr[0];
+    
+    for(int i = 1; i<n; i++){
+        if(arr[i]>largest){
+        largest = arr[i];
+        }
+        
+    }
+    cout<<"Largest element is "<<" "<<largest;
+}
